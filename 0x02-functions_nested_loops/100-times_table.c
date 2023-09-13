0300a6b47e74fff311d4d22f531ae452cc7204a5
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:57:23 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,69 +1,51 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table - This is a absolute value
  *
- * FUNCTIONALITY *
+ * @n: This is the entry
  *
- * 1. First, we check if the number is between 0 and 15. If it is, we continue.
- * 2. We create three variables: i, j, and k. i is the row, j is the column, and k is the product of i and j.
- * 3. We create a for loop that will iterate through the rows.
- * 4. We create a nested for loop that will iterate through the columns.
- * 5. We multiply i and j and store the result in k.
- * 6. We check if j is 0. If it is, we print k.
- * 7. If k is less than 10 and j is not 0, we print a comma, three spaces, and k.
- * 8. If k is greater than or equal to 10 and less than 100, we print a comma, two spaces, and k.
- * 9. If k is greater than or equal to 100, we print a comma, one space, and k.
- * 10. We print a new line.
- * 11. We close the nested for loop.
- * 12. We close the for loop.
- * 13. We close the if statement.
- * 14. We close the function.
- *
- * Time Complexity: O(n^2)
+ * Return: Always 0.
  */
 
 void print_times_table(int n)
 {
-	int i, j, k;
-
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		int b, c, d;
+		for (c = 0; c <= n; c++)
 		{
-			for (j = 0; j <= n; j++)
+			for (b = 0; b <= n; b++)
 			{
-				k = j * i;
-				if (j == 0)
+				d = b * c;
+
+				if (b == 0)
+					_putchar(d + '0');
+				else if (d <= 9)
 				{
-					_putchar(k + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(d + '0');
 				}
-				else if (k < 10 && j != 0)
+				else if (d >= 10 && d <= 99)
+				{
+					_putchar(32);
+					_putchar(((d / 10) % 10) + '0');
+					_putchar((d % 10) + '0');
+				}
+				else
+				{
+					_putchar((d / 100) + '0');
+					_putchar(((d / 10) % 10) + '0');
+					_putchar((d % 10) + '0');
+				}
+				if (b != n)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(32);
 				}
 			}
-			_putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
