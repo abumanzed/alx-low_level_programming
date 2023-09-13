92a3111aaf2a6937a0c5bcfcc7209b1a68ff415d
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:54:14 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * followed by a new lin
- * @n: print from this number
+ * print_to_98 - check the the entry to deteminate if is lower or upper
  *
- * FUNCTIONALITY *
- *
- * 1. If the number is less than 98, it will print all the numbers from the given number to 98.
- * 2. If the number is greater than 98, it will print all the numbers from the given number to 98.
- * 3. If the number is equal to 98, it will print 98.
- *
- * Time Complexity: O(n)
+ * @n: This is the entry
+ * Return: Always 0.
  */
+
 void print_to_98(int n)
 {
-
-	int i, j;
-
 	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
-		}
-	}
-	else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
-	}
+		for (; n <= 97; n++)
+			printf("%d, ", n);
+	else
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
