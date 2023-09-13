b0690cf3253805a75eade943b203ae4c10c70add
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:26:24 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -28,19 +28,18 @@
 */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-	_putchar('+');
-	return (1);
-	}
-	else if (n < 0)
-	{
-	_putchar('-');
-	return (-1);
-	}
-	else
-	{
-	_putchar('0');
-	return (0);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);}
 }
