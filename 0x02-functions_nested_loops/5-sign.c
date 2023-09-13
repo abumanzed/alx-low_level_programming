f981812a79c46ce1424c16a478ab0aeef91458b1
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:33:57 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -26,20 +26,24 @@
  * Returns:
 	The function is returning the sign of the number.
 */
+
 int print_sign(int n)
+
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-else
-{
-_putchar('0');
-return (0);}
+	
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
 }
