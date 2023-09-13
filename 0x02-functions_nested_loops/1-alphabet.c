eab4cb523a03696bd15d49f4e10502f1d82bf3a4
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:23:32 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,8 @@
  */
 void print_alphabet(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-	_putchar(letter);
-	}
-	_putchar('\n');
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
 }
