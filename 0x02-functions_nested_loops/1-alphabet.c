c58348f0c265f2c30c73bf520610a3cff3b6825e
abumanzed <zemedeabush54@gmail.com>
2023-09-13 02:57:29 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,11 @@
 /**
  * print_alphabet - Write a function that prints the alphabet, in lowercase,
  * followed by a new
- * Return: Always */
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
         _putchar(letter);
