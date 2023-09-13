86768a5fb67e15829768f45c9ffe0290a7013a2b
abumanzed <zemedeabush54@gmail.com>
2023-09-13 05:49:01 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,41 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - It is a table multiplication
  *
- * FUNCTIONALITY *
  *
- * 1. Declare the variables i, j, and k.
- * 2. Create a for loop that will iterate through the rows of the table.
- * 3. Create a nested for loop that will iterate through the columns of the table.
- * 4. Multiply the row and column to get the product.
- * 5. If the column is 0, print the product.
- * 6. If the product is less than 10 and the column is not 0, print a comma, two spaces, and the product.
- * 7. If the product is greater than or equal to 10, print a comma, a space, the tens digit of the product, and the ones digit of the product.
- * 8. Print a new line after each row.
- *
- * Time Complexity: O(n^2)
+ * Return: Always 0.
  */
+
 void times_table(void)
 {
-	int i, j, k;
+	int a, b, c;
 
-	for (i = 0; i < 10; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j < 10; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			k = j * i;
+			c = a * b;
 
-			if (j == 0)
+			if (b == 0)
 			{
-				_putchar(k + '0');
+				_putchar(c + '0');
 			}
-			if (k < 10 && j != 0)
+			else if (c <= 9)
 			{
-
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(32);
+				_putchar(c + '0');
 			}
-			else if (k >= 10)
+			else
 			{
-
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			if (b != 9)
+			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar(32);
 			}
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
