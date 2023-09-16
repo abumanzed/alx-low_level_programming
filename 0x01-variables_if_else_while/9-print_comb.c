f07197223be76e2d1b9426f9b420c7cdfd629450
abumanzed <zemedeabush54@gmail.com>
2023-09-15 22:46:05 -0700

    ./0-positive_or_negative

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,23 @@
-/*
- * File: 9-print_comb.c
- * Auth: Abusew Zemedikun
- */
-
 #include <stdio.h>
-
+#include <ctype.h>
 /**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
- */
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	int num;
+	int digit;
 
-	for (num = 0; num <= 9; num++)
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
+		putchar(digit);
+		if (digit == '9')
 			continue;
-		
+
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
