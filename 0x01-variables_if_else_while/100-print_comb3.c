378d87f22c58700e22a6bb33fb9b37b0973849e4
abumanzed <zemedeabush54@gmail.com>
2023-09-15 22:47:58 -0700

    ./0-positive_or_negative

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,10 @@
-/*
- * File: 10-print_comb2.c
- * Auth: Abusew Zemedikun
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
- */
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
 	int digit1, digit2;
@@ -19,18 +13,17 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
+
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
